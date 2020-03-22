0f7443aa1d9e2debe64b3f7fbba50d80868a967a
tangyufan <995435702@qq.com>
2020-03-22 21:14:04 +0800

    修改了消息消费

--- iot-pt/src/main/java/com/utils/amqp/AmqpRecev.java
+++ iot-pt/src/main/java/com/utils/amqp/AmqpRecev.java
@@ -8,9 +8,6 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 
 /**
 *   @desc : rabbitmq即使消息消费
