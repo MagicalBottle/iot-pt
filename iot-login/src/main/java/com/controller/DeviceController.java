23a6a8517c05e8786bebab10682dabc591053bdc
tangyufan <995435702@qq.com>
2020-03-24 00:03:39 +0800

    新增了html模板引擎

---
+++ iot-login/src/main/java/com/controller/DeviceController.java
@@ -0,0 +1,35 @@
+package com.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *   @desc : 设备信息
+ *   @auth : TYF
+ *   @date : 2020/3/16 - 23:35
+ */
+@Controller
+@RequestMapping("/device")
+public class DeviceController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     *   @desc : 获取token,服务器地址
+     *   @auth : TYF
+     *   @date : 2020/3/16 - 23:43
+     */
+    @RequestMapping("/info")
+    public String  login(HttpServletRequest req, HttpServletResponse resp) throws Exception{
+
+        return "info.html";
+    }
+
+
+
+}
